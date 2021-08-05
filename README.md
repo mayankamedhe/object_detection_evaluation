356a0d2614823529f07967e7705347ed682fb3a2
mayankamedhe <mayankaanilmedhe@gmail.com>
2021-08-05 17:46:24 +0900

    updated check_similarity function

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # object_detection_evaluation
+
+To run:
+
+At autoware.proj.gsm8-
+-	source install/setup.bash
+-	roslaunch perception_launch lidar_based_detection.launch
+-	roslaunch sensing_launch sensing.launch sensor_model:=aip_x2
+-	rosbag play --clock TC1-45mm-2_2021-05-17-15-01-18.bag
+-	rosrun rviz rviz -d src/autoware/launcher/autoware_launch/rviz/autoware.rviz
+
+At catkin_ws-
+To make-
+(In catkin_ws) - catkin_make
+
+-	source devel/setup.bash 
+- 	source /home/mayanka/autoware.proj.gsm8/install/setup.bash (for importing autoware_perception_msgs.msg)
+-	rosrun detection_evaluation scripts/evaluation.py
--- src/detection_evaluation/scripts/evaluation.py
+++ src/detection_evaluation/scripts/evaluation.py
@@ -1,65 +1,90 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-# from autoware_perception_msgs.msg.object_recognition import DynamicObjectWithFeatureArray # find documentation autoware_perception_msgs
-from visualization_msgs.msg import MarkerArray
+from autoware_perception_msgs.msg import DynamicObjectWithFeatureArray
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs import point_cloud2
+
+path_publisher = None
+previous_object_list = None
+object_ID = 0
+error = 0.1
+vehicle_velocity = 1
 
 
+### Function to check similarity between 2 objects based on their pose and shape
 
-def check_similarity(marker, previous_marker):
-    ## calcualte absolute pose of markers 
-    ## if difference is negligible -> same obj
+def check_similarity(current_object, previous_object, time_elapsed):
+
+    detected_object_info = [0, [0,0,0],[0,0,0]] ## [flag, pose, shape] where flag = 1 if objects are same
+
+    pose_current = [current_object.object.state.pose_covariance.pose.position.x, \
+                    current_object.object.state.pose_covariance.pose.position.y, \
+                    current_object.object.state.pose_covariance.pose.position.z]
+    pose_previous = [previous_object.object.state.pose_covariance.pose.position.x, \
+                    previous_object.object.state.pose_covariance.pose.position.y, \
+                    previous_object.object.state.pose_covariance.pose.position.z]
+
+    shape_current = [current_object.object.shape.dimensions.x, \
+                    current_object.object.shape.dimensions.y, \
+                    current_object.object.shape.dimensions.z]
+    shape_previous = [previous_object.object.shape.dimensions.x, \
+                     previous_object.object.shape.dimensions.y, \
+                     previous_object.object.shape.dimensions.z]
+
+    if abs(pose_current[0] - pose_previous[0]) <= error and \
+       abs(pose_current[2] - pose_previous[2]) <= error and \
+       abs(pose_current[1] - (pose_previous[1] - (vehicle_velocity*time_elapsed))) <= error and \
+       abs(shape_current[0] - shape_previous[0]) <= error and \
+       abs(shape_current[1] - shape_previous[1]) <= error and \
+       abs(shape_current[2] - shape_previous[2]) <= error:
+        
+        print("Same objects with pose = " + str(pose_current))
+        return [1, pose_current, shape_current]
+    
+    print("Different object pairs")
+    return [0, pose_current, shape_current]
 
 
-def callback(marker):
+### Callback function of the subscriber which parses the current object list and the previous one to check if the same object was detected consecutively 
+### and publishes the object ID, timestamp, pose, and shape. If the object is detected twice, it publishes the info only once
 
-    previous_marker = NULL
+
+def callback(current_object_list):
+
     detected_object = {}
-    object_ID = 0
-
-    if marker != NULL:
-        if previous_marker != NULL:
-            if check_similarity(marker, previous_marker) == 0:
-                object_ID = object_ID + 1
-
-            ## update detected object info
-            detected_object['object_ID'] = object_ID
-            detected_object['timestamp'] = marker.stamp
-            detected_object['pose'] = marker.pose
-
-            ## move to next marker
-            previous_marker = marker
-
-    self.path_publisher.publish(detected_object)
+    global previous_object_list
+    global object_ID
 
 
+    if current_object_list != None:
+        if previous_object_list != None:
+            for i in range(len(current_object_list.feature_objects)):
+                for j in range(len(previous_object_list.feature_objects)):
+                    time_elapsed = ((current_object_list.header.stamp.secs + (current_object_list.header.stamp.nsecs/1000000000)) - (previous_object_list.header.stamp.secs + (previous_object_list.header.stamp.nsecs/1000000000)))
+                    if check_similarity(current_object_list.feature_objects[i], previous_object_list.feature_objects[j], time_elapsed)[0] == 0:
+                        object_ID = object_ID + 1
 
-    # rospy.loginfo(rospy.get_caller_id() + "Cluster data %s", data.data)
-    # counter = 0;
-    # stamp_sec_prev = 0;
-    # stamp_nsec_prev = 0;
+                        ## update detected object info
+                        detected_object['object_ID'] = object_ID
+                        detected_object['timestamp'] = current_object_list.header.stamp
+                        detected_object['pose'] = current_object_list.feature_objects[i].object.state.pose_covariance.pose.position
+                        detected_object['shape'] = current_object_list.feature_objects[i].object.shape.dimensions
 
-    # if stamp_sec_prev == stamp_nsec_prev == 0: # No previous object has been detected yet -> keep account of time of 1st object
-    #     stamp_sec_prev = data.header.stamp.sec
-    #     stamp_nsec_prev = data.header.stamp.nsec
-    #     counter = counter + 1
-    # elif stamp_sec_prev - msg.header.stamp.sec <= 1: # new object detected within 1 second -> need to check if it is the same object
-    #     counter = counter + 1 # change (calculate previous distance and new distance -> check if within the bounds -> if yes = same object) 
-    # else:
-    #     stamp_sec_prev = 0
-    #     stamp_nsec_prev = 0
+                        # print(detected_object)
+                        detected_object_string = str(detected_object)
+                        path_publisher.publish(detected_object_string)
 
-    ## publish only certain fields- object pos, color, timestamp etc
-    self.path_publisher.publish(marker.data)
+        ## move to next object list
+        previous_object_list = current_object_list
+
     
 def listener():
+    global path_publisher
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/perception/object_recognition/detection/objects/visualization", MarkerArray, callback) # output/labeled_clusters
-
-    self.path_publisher = rospy.Publisher("object_detection/counter", MarkerArray, queue_size=1)
-
+    rospy.Subscriber("/objects", DynamicObjectWithFeatureArray, callback) # output/labeled_clusters, /perception/object_recognition/detection/objects/visualization -> MarkerArray,  /lidar_apollo_instance_segmentation/debug/instance_pointcloud -> PointCloud2
+    path_publisher = rospy.Publisher("object_detection/counter", String, queue_size=1)
     rospy.spin()
-
 
 if __name__ == '__main__':
     listener()
